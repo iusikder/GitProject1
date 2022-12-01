990d930ee70722e3cb93a13698def5d9b733010e
iusikder <iusikder@yahoo.com>
2022-11-30 21:42:54 -0500

    Second commit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,5 +2,7 @@ public class Test1 {
 
     public static void main(String [] args){
         System.out.println("Git project,,,,,,");
+        System.out.println("Push from Intellij.......");
+        System.out.println("Third commit .......");
     }
 }
